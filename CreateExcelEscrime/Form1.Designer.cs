8ec3ee71beb50a64b93dce59c2c01c6782d0c5b9
benoit lafaut <benoit.lafaut@gmail.com>
2025-11-02 22:03:51 +0100

    add

---
+++ CreateExcelEscrime/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace CreateExcelEscrime
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreateExcel = new System.Windows.Forms.Button();
+            this.btnComptabiliser = new System.Windows.Forms.Button();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.textBoxMontant = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxDestinataire = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxPériode = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBoxRaison = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnCreateWord = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCreateExcel
+            // 
+            this.btnCreateExcel.Location = new System.Drawing.Point(18, 246);
+            this.btnCreateExcel.Name = "btnCreateExcel";
+            this.btnCreateExcel.Size = new System.Drawing.Size(95, 42);
+            this.btnCreateExcel.TabIndex = 1;
+            this.btnCreateExcel.Text = "Create Excel";
+            this.btnCreateExcel.UseVisualStyleBackColor = true;
+            this.btnCreateExcel.Click += new System.EventHandler(this.btnCreateExcel_Click);
+            // 
+            // btnComptabiliser
+            // 
+            this.btnComptabiliser.Location = new System.Drawing.Point(257, 162);
+            this.btnComptabiliser.Name = "btnComptabiliser";
+            this.btnComptabiliser.Size = new System.Drawing.Size(189, 76);
+            this.btnComptabiliser.TabIndex = 2;
+            this.btnComptabiliser.Text = "Ajout dans la comptabilité";
+            this.btnComptabiliser.UseVisualStyleBackColor = true;
+            this.btnComptabiliser.Click += new System.EventHandler(this.btnComptabiliser_Click);
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.monthCalendar1.Location = new System.Drawing.Point(18, 18);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 3;
+            this.monthCalendar1.TitleForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.monthCalendar1.TrailingForeColor = System.Drawing.Color.Red;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // textBoxMontant
+            // 
+            this.textBoxMontant.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxMontant.Location = new System.Drawing.Point(347, 61);
+            this.textBoxMontant.Name = "textBoxMontant";
+            this.textBoxMontant.Size = new System.Drawing.Size(143, 24);
+            this.textBoxMontant.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(257, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Montant :";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // comboBoxDestinataire
+            // 
+            this.comboBoxDestinataire.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxDestinataire.FormattingEnabled = true;
+            this.comboBoxDestinataire.Location = new System.Drawing.Point(347, 21);
+            this.comboBoxDestinataire.Name = "comboBoxDestinataire";
+            this.comboBoxDestinataire.Size = new System.Drawing.Size(172, 26);
+            this.comboBoxDestinataire.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(257, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 26);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Destinataire :";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(257, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 26);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Période :";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // comboBoxPériode
+            // 
+            this.comboBoxPériode.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxPériode.FormattingEnabled = true;
+            this.comboBoxPériode.Location = new System.Drawing.Point(347, 130);
+            this.comboBoxPériode.Name = "comboBoxPériode";
+            this.comboBoxPériode.Size = new System.Drawing.Size(95, 26);
+            this.comboBoxPériode.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Location = new System.Drawing.Point(257, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 26);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Raison :";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // comboBoxRaison
+            // 
+            this.comboBoxRaison.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxRaison.FormattingEnabled = true;
+            this.comboBoxRaison.Location = new System.Drawing.Point(347, 95);
+            this.comboBoxRaison.Name = "comboBoxRaison";
+            this.comboBoxRaison.Size = new System.Drawing.Size(297, 26);
+            this.comboBoxRaison.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Location = new System.Drawing.Point(18, 179);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(227, 23);
+            this.label5.TabIndex = 13;
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnCreateWord
+            // 
+            this.btnCreateWord.Location = new System.Drawing.Point(18, 294);
+            this.btnCreateWord.Name = "btnCreateWord";
+            this.btnCreateWord.Size = new System.Drawing.Size(95, 42);
+            this.btnCreateWord.TabIndex = 14;
+            this.btnCreateWord.Text = "Create Word";
+            this.btnCreateWord.UseVisualStyleBackColor = true;
+            this.btnCreateWord.Click += new System.EventHandler(this.btnCreateWord_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 348);
+            this.Controls.Add(this.btnCreateWord);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBoxRaison);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBoxPériode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBoxDestinataire);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxMontant);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.btnComptabiliser);
+            this.Controls.Add(this.btnCreateExcel);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreateExcel;
+        private System.Windows.Forms.Button btnComptabiliser;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.TextBox textBoxMontant;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxDestinataire;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBoxPériode;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBoxRaison;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnCreateWord;
+    }
+}
+
---
+++ CreateExcelEscrime/Form1.cs
@@ -0,0 +1,275 @@
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using Application = System.Windows.Forms.Application;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace CreateExcelEscrime
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBoxDestinataire.Items.Clear();
+            comboBoxDestinataire.Items.Add("A.R.A.B.");
+            comboBoxDestinataire.Items.Add("Assurance DAS");
+            comboBoxDestinataire.Items.Add("Baptiste Motte");
+            comboBoxDestinataire.Items.Add("Club");
+            comboBoxDestinataire.Items.Add("Ecole Don Bosco");
+            comboBoxDestinataire.Items.Add("Eliot Brabant");
+            comboBoxDestinataire.Items.Add("Etat du compte");
+            comboBoxDestinataire.Items.Add("Fabrice Razanajao");
+            comboBoxDestinataire.Items.Add("Félix Trannoy");
+            comboBoxDestinataire.Items.Add("Frais bancaires");
+            comboBoxDestinataire.Items.Add("Ligue francophone");
+            comboBoxDestinataire.Items.Add("Oscar Deblocq");
+            comboBoxDestinataire.Items.Add("Planète Escrime");
+
+            comboBoxPériode.Items.Add("2025-2026");
+            comboBoxPériode.SelectedIndex = 0;
+
+            comboBoxRaison.Items.Add("Achat Matériel pour le club");
+            comboBoxRaison.Items.Add("Achat Matériel pour un tireur");
+            comboBoxRaison.Items.Add("Location Matériel");
+            comboBoxRaison.Items.Add("Payement à l'année");
+            comboBoxRaison.Items.Add("Payement 1er Carte");
+            comboBoxRaison.Items.Add("Payement 2ème Carte");
+            comboBoxRaison.Items.Add("Payement 3ème Carte");
+            comboBoxRaison.Items.Add("Payement 4ème Carte");
+            comboBoxRaison.Items.Add("Payement 5ème Carte");
+            comboBoxRaison.Items.Add("Payement 6ème Carte");
+            comboBoxRaison.Items.Add("Payement de la salle");
+            comboBoxRaison.Items.Add("Remboursement location matériel à un tireur");
+
+            label5.Text = DateTime.Now.ToShortDateString();
+        }
+
+        private void btnComptabiliser_Click(object sender, EventArgs e)
+        {
+            btnComptabiliser.Enabled = !btnComptabiliser.Enabled;
+            string path = Path.GetDirectoryName(Application.ExecutablePath) + @"\CreateExcel.txt";
+
+            string rowToAdd = monthCalendar1.SelectionRange.Start.ToString("yyyy-MM-dd") + ";"
+                + comboBoxDestinataire.Text + ";" + textBoxMontant.Text + ";" + comboBoxRaison.Text + ";" + comboBoxPériode.Text;
+
+            InsertText(path, rowToAdd);
+            btnComptabiliser.Enabled = !btnComptabiliser.Enabled;
+        }
+
+        public static void InsertText(string path, string newText)
+        {
+            if (File.Exists(path))
+            {
+                string oldText = File.ReadAllText(path);
+                using (var sw = new StreamWriter(path, false))
+                {
+                    sw.WriteLine(newText);
+                    sw.WriteLine(oldText);
+                }
+            }
+            else File.WriteAllText(path, newText);
+
+            string myFileData = File.ReadAllText(path);
+            File.WriteAllText(path, myFileData.TrimEnd(Environment.NewLine.ToCharArray()));
+        }
+
+        private void btnCreateExcel_Click(object sender, EventArgs e)
+        {
+            btnCreateExcel.Enabled = !btnCreateExcel.Enabled;
+
+            Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            object misValue = System.Reflection.Missing.Value;
+
+            xlWorkBook = xlApp.Workbooks.Add(misValue);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            xlWorkSheet.Cells[1, 1] = "Date";
+            xlWorkSheet.Cells[1, 2] = "Destinataire";
+            xlWorkSheet.Cells[1, 3] = "Montant Positif";
+            xlWorkSheet.Cells[1, 4] = "Montant Négatif";
+            xlWorkSheet.Cells[1, 5] = "Motif";
+            xlWorkSheet.Cells[1, 6] = "Période";
+            xlWorkSheet.Cells[1, 7] = "Etat du compte";
+
+            string path = Path.GetDirectoryName(Application.ExecutablePath) + @"\CreateExcel.txt";
+            string[] rows = File.ReadAllLines(path);
+
+            int rowIndew = 3;
+            foreach (string row in rows)
+            {
+                string[] cellule = row.Split(';');
+
+                xlWorkSheet.Cells[rowIndew, 1] = cellule[0];
+                xlWorkSheet.Cells[rowIndew, 1].NumberFormat = "[$-fr-BE]jjjj j mmmm aaaa";
+                xlWorkSheet.Cells[rowIndew, 2] = cellule[1];
+
+                if (cellule[1] == "Etat du compte")
+                {
+                    xlWorkSheet.Cells[rowIndew, 7] = cellule[2];
+                }
+                else
+                {
+                    if (decimal.Parse(cellule[2]) > 0)
+                    {
+                        xlWorkSheet.Cells[rowIndew, 3] = cellule[2];
+                    }
+                    else
+                    {
+                        if (decimal.Parse(cellule[2]) < 0)
+                        {
+
+                            xlWorkSheet.Cells[rowIndew, 4] = cellule[2];
+                        }
+
+                    }
+                    xlWorkSheet.Cells[rowIndew, 5] = cellule[3];
+                    xlWorkSheet.Cells[rowIndew, 6] = cellule[4];
+                }
+
+                rowIndew++;
+            }
+
+
+            Excel.Range usedrange = xlWorkSheet.UsedRange;
+            usedrange.Columns.AutoFit();
+
+            usedrange.Style.HorizontalAlignment = -4108;
+
+            string pathOutput = Path.GetDirectoryName(Application.ExecutablePath) + @"\ExportCreateExcel.xls";
+            File.Delete(pathOutput);
+
+            xlWorkBook.SaveAs(pathOutput, Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+            xlWorkBook.Close(true, misValue, misValue);
+            xlApp.Quit();
+
+            System.Diagnostics.Process.Start(pathOutput);
+            btnCreateExcel.Enabled = !btnCreateExcel.Enabled;
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            label5.Text = monthCalendar1.SelectionStart.Date.ToLongDateString() + "  (" + monthCalendar1.SelectionStart.Date.ToShortDateString() + ")";
+        }
+
+        private void btnCreateWord_Click(object sender, EventArgs e)
+        {
+            string path = Path.GetDirectoryName(Application.ExecutablePath) + @"\CreateExcel.txt";
+            string[] rows = File.ReadAllLines(path);
+
+            btnCreateWord.Enabled = !btnCreateWord.Enabled;
+
+            Microsoft.Office.Interop.Word.Application winword = new Microsoft.Office.Interop.Word.Application();
+            winword.ShowAnimation = false;
+            winword.Visible = false;
+            object missing = System.Reflection.Missing.Value;
+            Document document = winword.Documents.Add(ref missing, ref missing, ref missing, ref missing);
+            Paragraph paragraph1 = document.Content.Paragraphs.Add(ref missing);
+            object styleHeading1 = "Heading 1";
+            //paragraph1.Range.set_Style(ref styleHeading1);
+
+            paragraph1.Range.PageSetup.Orientation = WdOrientation.wdOrientLandscape;
+
+            Table firstTable = document.Tables.Add(paragraph1.Range, rows.Count(), 7, ref missing, ref missing);
+            firstTable.Borders.Enable = 1;
+
+
+
+            firstTable.Cell(1, 1).Range.Text = "Date";
+            firstTable.Cell(1, 2).Range.Text = "Destinataire";
+            firstTable.Cell(1, 3).Range.Text = "Montant Positif";
+            firstTable.Cell(1, 4).Range.Text = "Montant Négatif";
+            firstTable.Cell(1, 5).Range.Text = "Motif";
+            firstTable.Cell(1, 6).Range.Text = "Période";
+            firstTable.Cell(1, 7).Range.Text = "Etat du compte";
+
+            int rowIndew = 3;
+            foreach (string row in rows)
+            {
+                string[] cellule = row.Split(';');
+                var n = DateTime.Parse(cellule[0]).ToString("dddd dd-MMMM-yy");
+
+                firstTable.Cell(rowIndew, 1).Range.Text = FormatDay(n);
+                firstTable.Cell(rowIndew, 2).Range.Text = cellule[1];
+
+                if (cellule[1] == "Etat du compte")
+                {
+                    firstTable.Cell(rowIndew, 7).Range.Text = cellule[2];
+                }
+                else
+                {
+                    if (decimal.Parse(cellule[2]) > 0)
+                    {
+                        firstTable.Cell(rowIndew, 3).Range.Text = cellule[2];
+                    }
+                    else
+                    {
+                        if (decimal.Parse(cellule[2]) < 0)
+                        {
+
+                            firstTable.Cell(rowIndew, 4).Range.Text = cellule[2];
+                        }
+
+                    }
+                }
+
+                firstTable.Cell(rowIndew, 5).Range.Text = cellule[3];
+                firstTable.Cell(rowIndew, 6).Range.Text = cellule[4];
+
+                firstTable.Cell(rowIndew, 1).Range.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
+                firstTable.Cell(rowIndew, 2).Range.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
+                firstTable.Cell(rowIndew, 3).Range.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
+                firstTable.Cell(rowIndew, 4).Range.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
+                firstTable.Cell(rowIndew, 5).Range.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
+                firstTable.Cell(rowIndew, 6).Range.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
+
+                firstTable.Cell(rowIndew, 1).Range.Font.Size = 10;
+
+                rowIndew++;
+            }
+
+            firstTable.Range.Columns[7].AutoFit();
+            firstTable.Range.Columns[6].AutoFit();
+            firstTable.Range.Columns[5].AutoFit();
+            firstTable.Range.Columns[4].AutoFit();
+            firstTable.Range.Columns[3].AutoFit();
+            firstTable.Range.Columns[2].AutoFit();
+            firstTable.Range.Columns[1].AutoFit();
+
+
+
+
+            object filename = Path.GetDirectoryName(Application.ExecutablePath) + @"\ExportCreateWord.docx";
+            document.SaveAs2(ref filename);
+            document.Close(ref missing, ref missing, ref missing);
+            document = null;
+            winword.Quit(ref missing, ref missing, ref missing);
+            winword = null;
+            // MessageBox.Show("Document created successfully !");
+            System.Diagnostics.Process.Start(filename.ToString());
+
+            btnCreateWord.Enabled = !btnCreateWord.Enabled;
+        }
+
+        private string FormatDay(string n)
+        {
+            n = n.Replace("lundi", "Lun ");
+            n = n.Replace("mardi", "Mar ");
+            n = n.Replace("mercredi", "Mer ");
+            n = n.Replace("jeudi", "Jeu ");
+            n = n.Replace("vendredi", "Ven ");
+            n = n.Replace("samedi", "Sam ");
+            n = n.Replace("dimanche", "Dim ");
+            return n;
+        }
+    }
+}
